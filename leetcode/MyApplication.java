0ee96f00a5e12f9878c34d074c74436b677eada2
Anna Dvorkin <anna.dvorkin@teampicnic.com>
2025-04-04 08:19:39 +0000

    isValidAnagram

--- leetcode/MyApplication.java
+++ leetcode/MyApplication.java
@@ -2,13 +2,25 @@ package leetcode;
 public class MyApplication{
 
     public static void main(String[] args) {
+        // Test the validAnagram method
+        String s1 = "anagram";
+        String t1 = "nagaram";
+
+        String s2 = "rat";
+        String t2 = "car";
+
+        System.out.println("\n Is Valid Anagram");
+        System.out.println("IN: s=" + s1 + "t=" + t1 + "\n" + "EXP: true, " + "OUT: " + Strings.validAnagram(s1, t1) + "\n");
+        System.out.println("IN: s=" + s2 + "t=" + t2 + "\n" + "EXP: false, " + "OUT: " + Strings.validAnagram(s2, t2) + "\n");
+        
+
 
         // Test the validPolindrom method
         String input1 = "A man, a plan, a canal: Panama";
         String input2 = "race a car";
         String input3 = " ";
 
-        System.out.println(" ");
+        System.out.println("\n Is Valid Polindrom");
         System.out.println("IN: " + input1 + "\n" + "EXP: true, " + "OUT: " + Strings.validPolindrom(input1) + "\n");
         System.out.println("IN: " + input2 + "\n" + "EXP: false, " + "OUT: " + Strings.validPolindrom(input2) + "\n");
         System.out.println("IN: " + input3 + "\n" + "EXP: true, " + "OUT: " + Strings.validPolindrom(input3) + "\n");
--- leetcode/Strings.java
+++ leetcode/Strings.java
@@ -1,6 +1,37 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Strings {
+
+    public static boolean validAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        if(s.length() != t.length()) { 
+            return false;
+        }
+
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()) {
+            Integer count = map.get(c);
+            if(count != null && count > 0) {
+                map.put(c, count - 1);   
+                if(map.get(c) == 0){
+                    map.remove(c);
+                }
+            }
+
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    
 
     public static boolean validPolindrom(String s) {
                 int n = s.length();
