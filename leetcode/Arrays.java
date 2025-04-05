5a92ea49747fa7fefd34c4b76f0a2f09f41b859a
Anna Dvorkin <anna.dvorkin@teampicnic.com>
2025-04-05 19:41:40 +0000

    more

--- leetcode/Arrays.java
+++ leetcode/Arrays.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class Arrays {
 
+    // return the number which's count is greater than n/2
     public int majorityElement(int[] nums) {
         int n = nums.length;
         int minCount = n/2;
@@ -20,5 +21,36 @@ public class Arrays {
             visited.put(num, count);
         }
         return -1;        
-    } 
+    }
+    
+    // remove element equals to val
+    // return the number of elements remaining
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for(int number : nums) {
+            if(number != val) {                
+                nums[k] = number;
+                k++;
+            }         
+        }
+
+        return k;
+    }
+
+    // remove duplicates from sorted array
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int k = 1;
+
+        for(int i = 1; i < n; i++) {
+            if(nums[i] != nums[i-1])
+            {
+                nums[k] = nums[i]; 
+                k++;
+            }
+        }
+
+        return k;
+    }
 }
